1d2a20dbc4ba9102fe3f28142cb8ebecfa16e380
oscarhojs <osah@kth.se>
2024-09-05 14:54:42 +0200

    Create Trollkarlen_Array.py

---
+++ Trollkarlen_Array.py
@@ -0,0 +1,19 @@
+from ArrayQFile import ArrayQ
+
+def main():
+    kort = (input('Kort: '))
+    tmp = (kort.split(sep=' '))
+    lista = [int(obj) for obj in tmp]
+
+    print(lista)
+
+    q = ArrayQ(tmp)
+    mystring = []
+
+    while q.isEmpty() == False:
+        q.enqueue(q.dequeue())
+        mystring.append(q.dequeue()) 
+    
+    print(mystring)
+
+main()
