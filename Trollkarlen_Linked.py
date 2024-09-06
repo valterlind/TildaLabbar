e573b98d81c7ebffc96a71f2bd2fb9e5b5b2a2c9
valterlind <valter.lindstrom1@gmail.com>
2024-09-06 10:51:13 +0200

    Update Trollkarlen_Linked.py

--- Trollkarlen_Linked.py
+++ Trollkarlen_Linked.py
@@ -1,1 +1,20 @@
 
+from linkedQFile import LinkedQ
+
+def main():
+    kort = (input('Kort: '))
+    tmp = (kort.split(sep=' '))
+    lista = [int(obj) for obj in tmp]
+
+    print(lista)
+
+    q = ArrayQ(tmp)
+    mystring = []
+
+    while q.isEmpty() == False:
+        q.enqueue(q.dequeue())
+        mystring.append(q.dequeue()) 
+    
+    print(mystring)
+
+main()
