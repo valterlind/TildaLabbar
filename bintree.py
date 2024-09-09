859a852540def92cd48b0d4cf8f2c021e8bb7015
valterlind <valter.lindstrom1@gmail.com>
2024-09-09 20:09:54 +0200

    Create bintree.py

---
+++ bintree.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left  = None
+
+class Bintree:
+    def __init__(self):
+        self.root = None
+
+    def put(self,newvalue):
+        # Sorterar in newvalue i trädet
+        self.root = putta(self.root,newvalue)
+
+    def __contains__(self,value):
+        # True om value finns i trädet, False annars
+        return finns(self.root,value)
+
+    def write(self):
+        # Skriver ut trädet i inorder
+        skriv(self.root)
+        print("\n")
