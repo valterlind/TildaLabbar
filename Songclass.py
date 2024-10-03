113bd1a96071c4d769cb255b6963ab938352df47
valterlind <valter.lindstrom1@gmail.com>
2024-10-03 16:37:44 +0200

    Update Songclass.py

--- Songclass.py
+++ Songclass.py
@@ -9,11 +9,7 @@ class Song:
         return(f'Track ID: {self.trackid}\n Låt ID: {self.låtid}\n Artist: {self.artistnamn}\n Titel: {self.låttitel}')
 
     def __lt__(self, other):
-        """
-        Jämför två Song-objekt baserat på artistnamn (alfabetiskt).
-        :param other: Ett annat Song-objekt.
-        :return: True om artistnamnet på self är mindre än artistnamnet på other, annars False.
-        """
+        """ Jämför två Song-objekt baserat på artistnamn (alfabetiskt). """
         return self.artistnamn < other.artistnamn
     print(f"Sökning i hashtabell: {hash_result}, Tid: {hash_time:.6f} sekunder")
 
