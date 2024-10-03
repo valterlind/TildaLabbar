997776922d3edf53cce700198cf1d3cd0239b8fd
valterlind <valter.lindstrom1@gmail.com>
2024-10-03 16:42:29 +0200

    Update Songclass.py

--- Songclass.py
+++ Songclass.py
@@ -11,7 +11,7 @@ class Song:
     def __lt__(self, other):
         """ Jämför två Song-objekt baserat på artistnamn (alfabetiskt). """
         return self.artistnamn < other.artistnamn
-    print(f"Sökning i hashtabell: {hash_result}, Tid: {hash_time:.6f} sekunder")
+    
 
 if __name__ == "__main__":
     main()
