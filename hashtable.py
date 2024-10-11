32da4367466887f2b2b77c85a274c4dd91c7593d
valterlind <valter.lindstrom1@gmail.com>
2024-10-11 15:02:52 +0200

    Update hashtable.py

--- hashtable.py
+++ hashtable.py
@@ -1,9 +1,6 @@
 class HashNode:
-    """Noder till klassen Hashtable """
 
     def __init__(self, key="", data=None):
-        """key är nyckeln som används vid hashningen
-           data är det objekt som ska hashas in"""
         self.key = key
         self.data = data
         self.next = None  # Länkad lista för att hantera krockar
@@ -16,12 +13,8 @@ class Hashtable:
         self.table = [None] * size  # Skapa en tabell med angiven storlek
 
     def store(self, key, data):
-        """key är nyckeln
-           data är objektet som ska lagras
-           Stoppar in "data" med nyckeln "key" i tabellen."""
-        index = self.hashfunction(key)  # Beräkna hashindex baserat på nyckeln
+        index = self.hashfunction(key)
 
-        # Om platsen i tabellen är tom, lagra direkt
         if self.table[index] is None:
             self.table[index] = HashNode(key, data)
         else:
@@ -46,7 +39,7 @@ class Hashtable:
         current = self.table[index]
         while current:
             if current.key == key:
-                return current.data  # Returnera datat om nyckeln hittas
+                return current.data  # Returnera datan om nyckeln hittas
             current = current.next  # Gå till nästa nod om det finns en krock
 
         # Om vi inte hittade nyckeln
@@ -55,20 +48,7 @@ class Hashtable:
     def hashfunction(self, key):
         """key är nyckeln
            Beräknar hashfunktionen för key"""
-        # Enkel hashfunktion som summerar ASCII-värdena för tecknen i nyckeln
         hash_value = 0
         for char in key:
             hash_value += ord(char)
-        return hash_value % self.size  # Använder modulus för att placera inom tabellstorleken
-
-    def __getitem__(self, key):
-        """Gör det möjligt att använda [] för att slå upp ett värde"""
-        return self.search(key)
-
-    def __contains__(self, key):
-        """Gör det möjligt att använda 'key in hashtable' för att kolla om en nyckel finns"""
-        try:
-            self.search(key)
-            return True
-        except KeyError:
-            return False
+        return hash_value % self.size  
